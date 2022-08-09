d93cfd37288926767f74df9eec99d7357d016f8e
TommasoBelluzzo <tommaso.belluzzo@gmail.com>
2022-08-10 00:19:15 +0200

    Tests Rework

--- Solution/FastHashes.Tests/HashTests.cs
+++ Solution/FastHashes.Tests/HashTests.cs
@@ -14,35 +14,35 @@ namespace FastHashes.Tests
         #region Test Cases
         private static readonly List<TestCase> s_TestCases = new List<TestCase>
         {
-            new TestCase("FarmHash32", seed => new FarmHash32(seed), 0x0DC9AF39u),
-            new TestCase("FarmHash64", seed => new FarmHash64(seed), 0xEBC4A679u),
-            new TestCase("FarmHash128", seed => new FarmHash128(seed), 0xA93EBF71u),
-            new TestCase("FastHash32", seed => new FastHash32(seed), 0xE9481AFCu),
-            new TestCase("FastHash64", seed => new FastHash64(seed), 0xA16231A7u),
-            new TestCase("FastPositiveHash-V0", seed => new FastPositiveHash(FastPositiveHashVariant.V0, seed), 0x7F7D7B29u),
-            new TestCase("FastPositiveHash-V1", seed => new FastPositiveHash(FastPositiveHashVariant.V1, seed), 0xD6836381u),
-            new TestCase("FastPositiveHash-V2", seed => new FastPositiveHash(FastPositiveHashVariant.V2, seed), 0x8F16C948u),
-            new TestCase("HalfSipHash", seed => new HalfSipHash(seed), 0xDA2A194Cu),
-            new TestCase("HighwayHash64", seed => new HighwayHash64(seed), 0xCD809D2Du),
-            new TestCase("HighwayHash128", seed => new HighwayHash128(seed), 0x7C86214Cu),
-            new TestCase("HighwayHash256", seed => new HighwayHash256(seed), 0xF48F6052u),
-            new TestCase("MetroHash64-V1", seed => new MetroHash64(MetroHashVariant.V1, seed), 0xEE88F7D2u),
-            new TestCase("MetroHash64-V2", seed => new MetroHash64(MetroHashVariant.V2, seed), 0xE1FC7C6Eu),
-            new TestCase("MetroHash128-V1", seed => new MetroHash128(MetroHashVariant.V1, seed), 0x20E8A1D7u),
-            new TestCase("MetroHash128-V2", seed => new MetroHash128(MetroHashVariant.V2, seed), 0x5437C684u),
-            new TestCase("MurmurHash32", seed => new MurmurHash32(seed), 0xB0F57EE3u),
-            new TestCase("MurmurHash64-X86", seed => new MurmurHash64(MurmurHashEngine.x86, seed), 0x33E834ECu),
-            new TestCase("MurmurHash64-X64", seed => new MurmurHash64(MurmurHashEngine.x64, seed), 0x443D8C6Du),
-            new TestCase("MurmurHash128-X86", seed => new MurmurHash128(MurmurHashEngine.x86, seed), 0xB3ECE62Au),
-            new TestCase("MurmurHash128-X64", seed => new MurmurHash128(MurmurHashEngine.x64, seed), 0x6384BA69u),
-            new TestCase("MumHash", seed => new MumHash(seed), 0xA973C6C0u),
-            new TestCase("SipHash-13", seed => new SipHash(SipHashVariant.V13, seed), 0xF694F5B2u),
-            new TestCase("SipHash-24", seed => new SipHash(SipHashVariant.V24, seed), 0xBC31DC92u),
-            new TestCase("SpookyHash32", seed => new SpookyHash32(seed), 0x3F798BBBu),
-            new TestCase("SpookyHash64", seed => new SpookyHash64(seed), 0xA7F955F1u),
-            new TestCase("SpookyHash128", seed => new SpookyHash128(seed), 0x8D263080u),
-            new TestCase("xxHash32", seed => new XxHash32(seed), 0xBA88B743u),
-            new TestCase("xxHash64", seed => new XxHash64(seed), 0x024B7CF4u)
+            new TestCase("FarmHash32", seed => new FarmHash32(seed), "0x39AFC90D00000000"),
+            new TestCase("FarmHash64", seed => new FarmHash64(seed), "0x79A6C4EB62E22A5C"),
+            new TestCase("FarmHash128", seed => new FarmHash128(seed), "0x71BF3EA9A4D9F2351533AF0F4A96CEFB"),
+            new TestCase("FastHash32", seed => new FastHash32(seed), "0xFC1A48E9"),
+            new TestCase("FastHash64", seed => new FastHash64(seed), "0xA73162A1DA6BDCBD"),
+            new TestCase("FastPositiveHash-V0", seed => new FastPositiveHash(FastPositiveHashVariant.V0, seed), "0x297B7D7F81EC046C"),
+            new TestCase("FastPositiveHash-V1", seed => new FastPositiveHash(FastPositiveHashVariant.V1, seed), "0x816383D664638705"),
+            new TestCase("FastPositiveHash-V2", seed => new FastPositiveHash(FastPositiveHashVariant.V2, seed), "0x48C9168F5572BC11"),
+            new TestCase("HalfSipHash", seed => new HalfSipHash(seed), "0x4C192ADA"),
+            new TestCase("HighwayHash64", seed => new HighwayHash64(seed), "0x2D9D80CDBD9F39E2"),
+            new TestCase("HighwayHash128", seed => new HighwayHash128(seed), "0x4C21867CD2DD8E1882AC86A53705C53C"),
+            new TestCase("HighwayHash256", seed => new HighwayHash256(seed), "0x52608FF47620F0694CC650484BF6C530A05CDEC658AF6CE27667F8F75729FED8"),
+            new TestCase("MetroHash64-V1", seed => new MetroHash64(MetroHashVariant.V1, seed), "0xD2F788EE75E62D6B"),
+            new TestCase("MetroHash64-V2", seed => new MetroHash64(MetroHashVariant.V2, seed), "0x6E7CFCE14A183741"),
+            new TestCase("MetroHash128-V1", seed => new MetroHash128(MetroHashVariant.V1, seed), "0xD7A1E820E745C1A85B293EF5C2C876BC"),
+            new TestCase("MetroHash128-V2", seed => new MetroHash128(MetroHashVariant.V2, seed), "0x84C63754893C3AC63BAE5D82559AB27C"),
+            new TestCase("MurmurHash32", seed => new MurmurHash32(seed), "0xE37EF5B0"),
+            new TestCase("MurmurHash64-X86", seed => new MurmurHash64(MurmurHashEngine.x86, seed), "0xEC34E83397E0F193"),
+            new TestCase("MurmurHash64-X64", seed => new MurmurHash64(MurmurHashEngine.x64, seed), "0x6D8C3D44B40A4391"),
+            new TestCase("MurmurHash128-X86", seed => new MurmurHash128(MurmurHashEngine.x86, seed), "0x2AE6ECB33A231AD6384F94C332CAC079"),
+            new TestCase("MurmurHash128-X64", seed => new MurmurHash128(MurmurHashEngine.x64, seed), "0x69BA846303DEF3632DED84E68C879271"),
+            new TestCase("MumHash", seed => new MumHash(seed), "0xC0C673A922821163"),
+            new TestCase("SipHash-13", seed => new SipHash(SipHashVariant.V13, seed), "0xB2F594F65E5F1529"),
+            new TestCase("SipHash-24", seed => new SipHash(SipHashVariant.V24, seed), "0x92DC31BC9483210F"),
+            new TestCase("SpookyHash32", seed => new SpookyHash32(seed), "0xBB8B793F"),
+            new TestCase("SpookyHash64", seed => new SpookyHash64(seed), "0xF155F9A7B939EE62"),
+            new TestCase("SpookyHash128", seed => new SpookyHash128(seed), "0x8030268DFD1A1430978CC2342FFEDAAA6D08879F228067F71E74FF83D5903E3DE7D57A67C25848034C46C21EB8361F59B08A199D9B25B73EFF7D4A9233A7C701F54331F2293E674524CD1A9D8550A1270473AE2C1925EAB0DA9F2B641458E626"),
+            new TestCase("xxHash32", seed => new XxHash32(seed), "0x43B788BA00000000"),
+            new TestCase("xxHash64", seed => new XxHash64(seed), "0xF47C4B0200F12795")
         };
 
         public static IEnumerable<Object[]> DataCollision()
@@ -107,7 +107,7 @@ namespace FastHashes.Tests
 
         [Theory]
         [MemberData(nameof(DataValidation))]
-        public void ValidationTest(String hashName, UInt32 expectedValue)
+        public void ValidationTest(String hashName, String expectedValue)
         {
             Func<UInt32,Hash> hashInitializer = s_TestCases.Single(x => x.HashName == hashName).HashInitializer;
 
@@ -128,7 +128,7 @@ namespace FastHashes.Tests
             }
         
             Byte[] h0 = hash0.ComputeHash(bufferFinal);   
-            UInt32 actualValue = (UInt32)((h0[0] << 0) | (h0[1] << 8) | (h0[2] << 16) | (h0[3] << 24));
+            String actualValue = String.Concat("0x", BitConverter.ToString(h0).Replace("-", String.Empty));
 
             m_Output.WriteLine($"EXPECTED={expectedValue}");
             m_Output.WriteLine($"ACTUAL={actualValue}");
@@ -143,17 +143,17 @@ namespace FastHashes.Tests
             #region Members
             private readonly Func<UInt32,Hash> m_HashInitializer;
             private readonly String m_HashName;
-            private readonly UInt32 m_ExpectedValue;
+            private readonly String m_ExpectedValue;
             #endregion
 
             #region Properties
             public Func<UInt32,Hash> HashInitializer => m_HashInitializer;
             public String HashName => m_HashName;
-            public UInt32 ExpectedValue => m_ExpectedValue;
+            public String ExpectedValue => m_ExpectedValue;
             #endregion
 
             #region Constructors
-            public TestCase(String hashName, Func<UInt32,Hash> hashInitializer, UInt32 expectedValue)
+            public TestCase(String hashName, Func<UInt32,Hash> hashInitializer, String expectedValue)
             {
                 if (String.IsNullOrWhiteSpace(hashName))
                     throw new ArgumentException("Invalid hash name specified.", nameof(hashName));
@@ -161,7 +161,7 @@ namespace FastHashes.Tests
                 if (hashInitializer == null)
                     throw new ArgumentException("Invalid hash initializer specified.", nameof(hashInitializer));
 
-                if (expectedValue == 0u)
+                if (String.IsNullOrWhiteSpace(hashName))
                     throw new ArgumentException("Invalid expected value specified.", nameof(expectedValue));
 
                 m_HashInitializer = hashInitializer;
